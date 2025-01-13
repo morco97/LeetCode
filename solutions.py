66f5cdff1497f5abbd761d170d429f8ba9eedac3
Mor Cohen <65893813+morco97@users.noreply.github.com>
2025-01-13 14:25:56 +0200

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -12,7 +12,16 @@ class Solution:
                   index -= 1
                   total -= 1
           index += 1
-  
+    
       return total if len(nums) >= 2 else 1
-
-  
+        
+    def findMaxK(self, nums: List[int]) -> int:
+        """
+        2441. Largest Positive Integer That Exists With Its Negative
+        """
+        check = set(nums)
+        maximal = -1
+        for num in nums:
+            if num > maximal and -1 * num in check:
+                maximal = num
+        return maximal
